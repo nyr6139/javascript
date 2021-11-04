8fbf43731e003de3a19953e849d230faf299dad3
nyr6139 <aksqh801@naver.com>
2021-11-04 23:12:18 +0900

    function-ex

---
+++ 함수/function-ex.js
@@ -0,0 +1,37 @@
+// *Function declaration
+function calcAge1(birthYear) {
+  return 2037 - birthYear;
+}
+
+const age1 = calcAge1(1991);
+
+// *Function expression
+const calcAge2 = function(birthYear) {
+  return 2037 - birthYear;
+}
+
+const age2 = calcAge2(1991);
+
+console.log(age1, age2)  // 46, 46
+
+// *Arrow function
+const calcAge3 = birthYear => 2037 - birthYear;
+const age3 = calcAge3(1991);
+console.log(age3);    // 46
+
+const yearsUntilRetirement = (birthYear, firstName) => {
+  const age = 2037 - birthYear;
+  const retirement = 65 - age;
+  // return retirement;
+  return `${firstName} retires in ${retirement} years`;
+}
+
+console.log(yearsUntilRetirement(1991, 'jonas'))  // jonas retires in 19 years
+console.log(yearsUntilRetirement(1980, 'bob'))    // bob retires in 8 years
+
+// * functions calling other functions
+
+
+
+
+
