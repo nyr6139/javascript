87f04da0fb3f79899af7040fdab63088e3f5da1d
nyr6139 <aksqh801@naver.com>
2021-11-04 23:30:43 +0900

    function-ex

--- 함수/function-ex.js
+++ 함수/function-ex.js
@@ -5,6 +5,9 @@ function calcAge1(birthYear) {
 
 const age1 = calcAge1(1991);
 
+
+
+
 // *Function expression
 const calcAge2 = function(birthYear) {
   return 2037 - birthYear;
@@ -13,6 +16,9 @@ const calcAge2 = function(birthYear) {
 const age2 = calcAge2(1991);
 
 console.log(age1, age2)  // 46, 46
+
+
+
 
 // *Arrow function
 const calcAge3 = birthYear => 2037 - birthYear;
@@ -29,8 +35,22 @@ const yearsUntilRetirement = (birthYear, firstName) => {
 console.log(yearsUntilRetirement(1991, 'jonas'))  // jonas retires in 19 years
 console.log(yearsUntilRetirement(1980, 'bob'))    // bob retires in 8 years
 
-// * functions calling other functions
 
+
+
+// * functions calling other functions
+function cutFruitPieces(fruit){
+  return fruit * 4
+}
+
+function fruitProcessor(apples, oranges){
+  const applePieces = cutFruitPieces(apples);
+  const orangePieces = cutFruitPieces(oranges);
+
+  const juice = `Juice with ${applePieces} piece of apple and ${orangePieces} pieces of orange.`;
+  return juice;
+}
+console.log(fruitProcessor(2, 3));  // Juice with 8 piece of apple and 12 pieces of orange.
 
 
 
