1d63a20c66cc4a8781191b9fde8675a6a3751365
nyr6139 <aksqh801@naver.com>
2021-11-08 23:48:10 +0900

    array

--- array/array.js
+++ array/array.js
@@ -1,4 +1,4 @@
-// SLICE
+// SLICE (배열 그대로 반환)
 let arr = ['a','b','c','d','e'];
 
 console.log(arr.slice(2));    // ['c','d','e']
@@ -9,5 +9,22 @@ console.log(arr.slice(1,-2));    // ['b','c']
 console.log(arr.slice());    // ['a','b','c','d','e']
 console.log([...arr]);    // ['a','b','c','d','e']
 
-// SPLICE
+// SPLICE (배열 변경되어 반환)
 console.log(arr.splice(2));   // ['c','d','e']
+// console.log(arr);   // ['a','b']
+console.log(arr.splice(-1));   // ['a','b','c','d']
+console.log(arr.splice(1,2));   // ['a','d','e']
+
+
+// REVERSE (배열 변경되어 반환)
+arr = ['a','b','c','d','e'];
+const arr2 = ['j','i','h','g','f']
+console.log(arr2.reverse());    // ['f','g','h','i','j']
+console.log(arr2);    // ['f','g','h','i','j'] 
+
+
+// CONCAT (배열 변경 x)
+const letters = arr.concat(arr2);   // ['a','b','c','d','e','f','g','h','i','j'];
+
+// JOIN
+console.log(letters.join('-'));     // a - b - c - d - e - f - g - h - i - j
